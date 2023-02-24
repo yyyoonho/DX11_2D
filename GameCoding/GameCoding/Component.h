9876e5b37b5977ccd9a7ff1d64862ba08236b80a
yyyoonho <parn997@naver.com>
2023-02-24 20:56:37 +0900

    feat: Component, Transform 추가

---
+++ GameCoding/GameCoding/Component.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class GameObject;
+
+class Component
+{
+public:
+	Component();
+	 virtual ~Component();
+
+	virtual void Init() abstract;
+	virtual void Update() abstract;
+
+	shared_ptr<GameObject> GetGameObject() { return _owner; }
+
+protected:
+	shared_ptr<GameObject> _owner;
+};
+
